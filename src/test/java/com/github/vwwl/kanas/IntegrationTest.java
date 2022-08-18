65c0f247ad290ca3f447e9b8836fc3ef3a4ca64b
Neil Wang <wangweili457@gmail.com>
2022-08-18 16:44:34 +0800

    first commit

---
+++ src/test/java/com/github/vwwl/kanas/IntegrationTest.java
@@ -0,0 +1,7 @@
+package com.github.vwwl.kanas;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public abstract class IntegrationTest {
+}
---
+++ src/test/java/com/github/vwwl/kanas/test/config/TestConfiguration.java
@@ -0,0 +1,7 @@
+package com.github.vwwl.kanas.test.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestConfiguration {
+}
