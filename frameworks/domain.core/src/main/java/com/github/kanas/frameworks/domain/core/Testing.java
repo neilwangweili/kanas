90be1190cb987e2d45e66a0ed953ce72197aa816
Neil Wang <wangweili457@gmail.com>
2022-08-23 15:22:41 +0800

    REFACTOERD: Rename Testing.content.

--- frameworks/domain.core/src/main/java/com/github/kanas/frameworks/domain/core/Testing.java
+++ frameworks/domain.core/src/main/java/com/github/kanas/frameworks/domain/core/Testing.java
@@ -4,21 +4,21 @@ import org.jetbrains.annotations.*;
 
 public final class Testing<Content> {
 
-    private final Content t;
+    private final Content content;
 
     public static <Content> @NotNull Testing<Content> by(@NotNull Content testing) {
         return new Testing<>(testing);
     }
 
-    public Testing(@NotNull final Content t) {
-        this.t = t;
+    public Testing(@NotNull final Content content) {
+        this.content = content;
     }
 
     @SuppressWarnings("unchecked")
     public <Field> @Nullable Field get(@NotNull final String fieldName) {
-        java.lang.reflect.Field field = Exceptions.evaluate(() -> t.getClass().getDeclaredField(fieldName));
+        java.lang.reflect.Field field = Exceptions.evaluate(() -> content.getClass().getDeclaredField(fieldName));
         field.setAccessible(true);
-        return (Field) Exceptions.evaluate(() -> field.get(t));
+        return (Field) Exceptions.evaluate(() -> field.get(content));
     }
 
 }
