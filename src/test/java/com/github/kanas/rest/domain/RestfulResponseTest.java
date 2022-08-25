44aee72b62c3e760d1b1a1c141e925d645f3eb62
Neil Wang <wangweili457@gmail.com>
2022-08-25 10:56:00 +0800

    TEST: should_get_response_value_when_giving_response_path

--- src/test/java/com/github/kanas/rest/domain/RestfulResponseTest.java
+++ src/test/java/com/github/kanas/rest/domain/RestfulResponseTest.java
@@ -1,8 +1,10 @@
 package com.github.kanas.rest.domain;
 
+import com.github.kanas.core.Testing;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RestfulResponseTest {
 
@@ -10,6 +12,12 @@ class RestfulResponseTest {
     void should_create_response() {
         RestfulResponse response = new RestfulResponse("{}");
         assertThat(response).isInstanceOf(Response.class);
+    }
+
+    @Test
+    void should_get_response_value_when_giving_response_path() {
+        RestfulResponse response = new RestfulResponse("{\"name\":\"admin\"}");
+        assertEquals("admin", Testing.by(response.body("$.name")).get("value"));
     }
 
 }
